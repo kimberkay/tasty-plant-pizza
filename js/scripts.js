551580aa0d7a19b0127a5a8368d8ee92e83fd054
Kim Brannian <brannian@gmail.com>
2021-11-05 10:11:26 -0700

    Add business logic to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 //Business Logic
 
+let testPizza = new Pizza ("medium",["pineapple"])
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
+Pizza.prototype.size = function() {
+  let price = 0;
+  if (this.size === "medium") {
+    return price + 22; 
+  } else if (this.size === "large") {
+    return price + 25;
+  }
+}
